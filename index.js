620df7d3974c56d37874b450e6faee5cff95b8b8
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-18 05:56:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+
 "use strict";
 
 const express = require("express");
 const bodyParser = require("body-parser");
+var http = require('http');
+var request = require('request');
 
 const restService = express();
 
 const App = require('actions-on-google').DialogflowApp;
 
 
-var obj = [];
-var myObj = [];
-var a;
-var i = 0;
+//var url = "http://208.85.249.174:8000/sap/opu/odata/CRVWM/WMS_SRV";
+var url = "http://208.85.249.174:8000/sap/opu/odata/CRVWM/WMS_SRV/";
 
+var d = '1140';
+var i = 0;
+var obj = [];
 restService.use(
   bodyParser.urlencoded({
       extended: true
@@ -23,12 +27,7 @@ restService.use(bodyParser.json());
 
 
 restService.post("/slack-test", function (req, res) {
-const app = new App({ request: req, response: res });
-
-    //const app = new App({req, res});
-
-    //const param = app.getContextArgument('actions_intent_option',
-    // 'OPTION').value;
+    const app = new App({ request: req, response: res });
 
     var speech =
       req.body.result &&
@@ -37,114 +36,111 @@ const app = new App({ request: req, response: res });
         : "wrong";
 
 
-    //var key=JSON.stringify(req.body);
 
-    var speech11 =
-      req.body.result &&
-      req.body.result.parameters &&
-      req.body.result.parameters.key
-        ? req.body.result.parameters.key
-        : "xx";
+    var csrfToken;
+    request({
+        //url: url + "/TOItemDetailsSet?$filter=ToNum eq('" + d + "')&$format=json",
+        url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
+        headers: {
+            "Authorization": "Basic <<base64 encoded sapuser:crave123>>",
+            "Content-Type": "application/json",
+            "x-csrf-token": "Fetch"
+        }
 
-    var myObj = [
-{
-    'CustomerID': "ALFKI",
-    'CompanyName': "Alfreds Futterkiste",
-    'ContactName': "Maria Anders"
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            csrfToken = response.headers['x-csrf-token'];
+            // console.log(csrfToken);
+            // var gwResponse = body.asString();
+            // var JSONObj = JSON.parse(body);
+            var c = JSON.parse(body)
+            //var a = res.json(body);
+            var len = c.d.results.length;
+            //var a = JSON.stringify(a);
 
+            var botResponse;
 
-},
-{
-    'CustomerID': "ANATR",
-    'CompanyName': "Ana Trujillo Emparedados y helados",
-    'ContactName': "Ana Trujillo"
+            if (c.d.results.length > 0) {
+                // botResponse = "Your latest Purchase orders are: ";
 
-}];
-
-    for (; i < myObj.length; i++) {
-
-        var tmp = {
-            'optionInfo': { 'key': myObj[i].CustomerID },
-            'title': myObj[i].CompanyName,
-            'description': myObj[i].ContactName
-        };
-
-        obj.push(tmp);
-
-    }
-	
-	    if (speech == "actions_intent_OPTION") {
-	        var param = app.getArgument('OPTION');
-	        var input = app.getRawInput();
-    }
-    else {
-	        var param = "hi";
-	        var input = "bye";
-    }
-
-    var slack_message = {
-
-        expect_user_response: true,
-        rich_response: {
-            items: [
-                  {
-                      simpleResponse: {
-                          textToSpeech: param
-                      }
-                  }
-            ],
-            suggestions: [
-				{
-				    title: "List"
-				},
-				{
-				    title: "Carousel"
-				},
-				{
-				    title: "Suggestions"
-				}
-            ]
-
-
-        },
-
-        systemIntent: {
-            intent: "actions.intent.OPTION",
-            data: {
-                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                listSelect: {
-                    title: "List Title",
-                    items: obj
+                for (; i < c.d.results.length; i++) {
+                    // botResponse += " ";
+                    botResponse = {
+                        
+                        'optionInfo': { 'key': c.d.results[i].ToNumerID },
+                        'title': c.d.results[i].ToNum
+            
+                    }
+                    obj.push(botResponse);
                 }
+            } else {
+                botResponse = "You do not seem to have any active Purchase Orders!";
             }
+            var slack_message = {
+
+                expect_user_response: true,
+                rich_response: {
+                    items: [
+                          {
+                              simpleResponse: {
+                                  textToSpeech: "You have below list of order"
+                              }
+                          }
+                    ],
+                    suggestions: [
+                        {
+                            title: "List"
+                        },
+                        {
+                            title: "Carousel"
+                        },
+                        {
+                            title: "Suggestions"
+                        }
+                    ]
+
+
+                },
+
+                systemIntent: {
+                    intent: "actions.intent.OPTION",
+                    data: {
+                        "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                        listSelect: {
+                            title: "Select the order",
+                            items: obj
+                        }
+                    }
+                }
+
+
+
+            };
+
+            return res.json({
+                speech: "",
+                displayText: "",
+
+                source: "webhook-echo-sample",
+
+                data: {
+                    google: slack_message
+                }
+
+
+
+            });
+
+            //console.log(JSON.stringify(obj));
         }
-
-
-
-    };
-
-
-    return res.json({
-        speech: "",
-        displayText: "",
-
-        source: "webhook-echo-sample",
-
-        data: {
-            google: slack_message
-        }
-
-
-
     });
 
-
-
+   
 
 });
-
 
 
 restService.listen(process.env.PORT || 8000, function () {
     console.log("Server up and listening");
 });
+
