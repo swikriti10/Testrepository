4c626c707eb72ed7fcc565e3398e5a2215660a6e
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-18 07:11:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,114 +28,168 @@ restService.use(bodyParser.json());
 
 restService.post("/slack-test", function (req, res) {
     const app = new App({ request: req, response: res });
-
-    var speech =
+    var csrfToken;
+    var actionName =
       req.body.result &&
       req.body.result.action
         ? req.body.result.action
         : "wrong";
 
+    if (actionName == "get_action") {
 
-
-    var csrfToken;
-    request({
-        //url: url + "/TOItemDetailsSet?$filter=ToNum eq('" + d + "')&$format=json",
-        url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
-        headers: {
-            "Authorization": "Basic <<base64 encoded sapuser:crave123>>",
-            "Content-Type": "application/json",
-            "x-csrf-token": "Fetch"
-        }
-
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            csrfToken = response.headers['x-csrf-token'];
-            // console.log(csrfToken);
-            // var gwResponse = body.asString();
-            // var JSONObj = JSON.parse(body);
-            var c = JSON.parse(body)
-            //var a = res.json(body);
-            var len = c.d.results.length;
-            //var a = JSON.stringify(a);
-
-            var botResponse;
-
-            if (c.d.results.length > 0) {
-                // botResponse = "Your latest Purchase orders are: ";
-
-                for (; i < c.d.results.length; i++) {
-                    // botResponse += " ";
-                    botResponse = {
-                        
-                        'optionInfo': { 'key': c.d.results[i].ToNum},
-                        'title': c.d.results[i].ToNum
-            
-                    }
-                    obj.push(botResponse);
-                }
-            } else {
-                botResponse = "You do not seem to have any active Purchase Orders!";
+        request({
+            //url: url + "/TOItemDetailsSet?$filter=ToNum eq('" + d + "')&$format=json",
+            url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
+            headers: {
+                "Authorization": "Basic <<base64 encoded sapuser:crave123>>",
+                "Content-Type": "application/json",
+                "x-csrf-token": "Fetch"
             }
-             slack_message = {
 
-                expect_user_response: true,
-                rich_response: {
-                    items: [
-                          {
-                              simpleResponse: {
-                                  textToSpeech: "You have below list of order"
-                              }
-                          }
-                    ],
-                    suggestions: [
-                        {
-                            title: "List"
-                        },
-                        {
-                            title: "Carousel"
-                        },
-                        {
-                            title: "Suggestions"
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                csrfToken = response.headers['x-csrf-token'];
+                // console.log(csrfToken);
+                // var gwResponse = body.asString();
+                // var JSONObj = JSON.parse(body);
+                var c = JSON.parse(body)
+                //var a = res.json(body);
+                var len = c.d.results.length;
+                //var a = JSON.stringify(a);
+
+                var botResponse;
+
+                if (c.d.results.length > 0) {
+                    // botResponse = "Your latest Purchase orders are: ";
+
+                    for (; i < c.d.results.length; i++) {
+                        // botResponse += " ";
+                        botResponse = {
+
+                            'optionInfo': { 'key': c.d.results[i].ToNumerID },
+                            'title': c.d.results[i].ToNum
+
                         }
-                    ]
-
-
-                },
-
-                systemIntent: {
-                    intent: "actions.intent.OPTION",
-                    data: {
-                        "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                        listSelect: {
-                            title: "Select the order",
-                            items: obj
-                        }
+                        obj.push(botResponse);
                     }
+
+                    slack_message = {
+
+                        expect_user_response: true,
+                        rich_response: {
+                            items: [
+                                  {
+                                      simpleResponse: {
+                                          textToSpeech: "You have below list of order"
+                                      }
+                                  }
+                            ],
+                            suggestions: [
+                                {
+                                    title: "List"
+                                },
+                                {
+                                    title: "Carousel"
+                                },
+                                {
+                                    title: "Suggestions"
+                                }
+                            ]
+
+
+                        },
+
+                        systemIntent: {
+                            intent: "actions.intent.OPTION",
+                            data: {
+                                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                                listSelect: {
+                                    title: "Select the order",
+                                    items: obj
+                                }
+                            }
+                        }
+
+
+
+                    };
+
+
+
+                } else {
+                    botResponse = "You do not seem to have any active Purchase Orders!";
+                    slack_message = {
+
+                        expect_user_response: true,
+                        rich_response: {
+                            items: [
+                                  {
+                                      simpleResponse: {
+                                          textToSpeech: botResponse
+                                      }
+                                  }
+                            ]
+
+
+                        }
+
+
+
+                    };
+
+
+
+
                 }
 
 
 
-            };
 
-           
+                //console.log(JSON.stringify(obj));
+            }
+        });
 
-            //console.log(JSON.stringify(obj));
-        }
-    });
+    }
+    else if (actionName == "actions_intent_OPTION") {
+
+        var param = app.getArgument('OPTION');
+
+        slack_message = {
+
+            expect_user_response: true,
+            rich_response: {
+                items: [
+                      {
+                          simpleResponse: {
+                              textToSpeech: param
+                          }
+                      }
+                ]
+
+
+            }
+
+
+
+        };
+
+    }
+   
+    
 
     return res.json({
-                speech: "",
-                displayText: "",
+        speech: "",
+        displayText: "",
 
-                source: "webhook-echo-sample",
+        source: "webhook-echo-sample",
 
-                data: {
-                    google: slack_message
-                }
+        data: {
+            google: slack_message
+        }
 
 
 
-            });
+    });
 
 });
 
