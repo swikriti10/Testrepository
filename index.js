e5aec06b714f55dcc4a7e55242fe7212182bf468
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-21 23:25:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ var slack_message = {
                 items: [
                       {
                           simpleResponse: {
-                              textToSpeech: "oyee2"
+                              textToSpeech:param
                           }
                       }
                 ]
